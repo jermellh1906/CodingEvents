bb5dcb55db5c523eaaef15acc64abe64803f5d56
Jermell Harris <jermellh@hotmail.com>
2021-01-28 15:58:48 -0600

    Add a Model Property

--- Controllers/EventsController.cs
+++ Controllers/EventsController.cs
@@ -29,9 +29,9 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name)
+        public IActionResult NewEvent(string name, string desc)
         {
-            Events.Add(new Event(name));
+            Events.Add(new Event(name, desc));
             return Redirect("/Events");
         }
     }
